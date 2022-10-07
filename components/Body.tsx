eaad65cc9969658e8a89d734ee0f439976c9a67d
avimi13 <alfredorequenez57libra@gmail.com>
2022-10-07 10:20:12 -0600

    Firths commit

---
+++ components/Body.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet, Text} from 'react-native'
+
+const Body =()=> {
+    return (
+        <View>
+            <Text>Hola</Text>
+        </View>
+    )
+}
+export default Body;
+
+const style= StyleSheet.create ({
+
+})
---
+++ components/Main.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { View, StyleSheet} from 'react-native'
+import Body from './Body';
+
+const Main =() => {
+    return(
+        <View>
+            <View>
+                
+            </View>
+            <View>
+
+            </View>
+            <Body/>
+            <View>
+
+            </View>
+            <View>
+
+            </View>
+        </View>
+    )
+}
+export default Main;
+const style= StyleSheet.create ({
+    
+})
